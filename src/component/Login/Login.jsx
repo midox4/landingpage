d7223d7d10eb7e5f706d91306ae99decd17156a9
MacBook <macbook@MacBook-Air-de-MacBook.local>
2024-02-15 00:42:15 +0100

    update2

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -10,9 +10,9 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import anim from "../../assets/img/anim2.json";
 import Lottie from "lottie-react";
-import "@fontsource/roboto/300.css"
-import "@fontsource/roboto/500.css"
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/500.css";
+import poste from "../../assets/img/logoposte.png";
 
 // TODO remove, this demo shouldn't need to reset the theme.
 
@@ -29,137 +29,162 @@ function Login() {
   return (
     <Box
       sx={{
-        margin:'20px',
+        margin: "20px",
+        padding: "10px",
         width: "97%",
         position: "relative",
-        borderRadius: "8px",
-        height: "94vh",
+        borderRadius: "20px",
+        height: "96vh",
+        bgcolor: "#e0f7fa",
         display: "flex",
         flexDirection: "column",
         justifyContent: "start",
         alignItems: "center",
       }}
     >
-    
-    <Typography  sx={{fontFamily:'Roboto',fontWeight:'500', fontSize:'30px', textAlign:'center'}}>
-         <b>
-         Explorez Notre Solution ERP <br /> dédiée au secteur postal, optimisée pour
-          répondre <br /> à vos besoins spécifiques.
-            </b> 
-            
-          <Box  sx={{textAlign:"center", fontFamily:'Roboto', marginTop:'8px', fontSize:'20px' , color:'gray'}}>
-          Découvrez comment notre plateforme
-          vous permet de gérer <br /> efficacement les opérations postales
-            </Box>
-            
-          <img src="" alt=""></img>
-        </Typography>
+      <Typography
+        sx={{
+          fontFamily: "Roboto",
+          fontWeight: "500",
+          fontSize: "30px",
+          textAlign: "center",
+        }}
+      >
+        <b>
+          Explorez Notre Solution ERP <br /> dédiée au secteur postal, optimisée
+          pour répondre <br /> à vos besoins spécifiques.
+        </b>
 
-        <Box   sx={{
-        width: "97%",
-        position: "relative",
-        height: "94vh",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "start",
-        alignItems: "start",
-
-      }}>
-        <Box sx={{ width: "50%", height: "100vh" }}>
         <Box
           sx={{
+            textAlign: "center",
+            fontFamily: "Roboto",
+            marginTop: "8px",
+            fontSize: "20px",
+            color: "#616161            ",
+          }}
+        >
+          Découvrez comment notre plateforme vous permet de gérer <br />{" "}
+          efficacement les opérations postales
+        </Box>
+
+        <img src="" alt=""></img>
+      </Typography>
+
+      <Box
+        sx={{
+          width: "97%",
+          position: "relative",
+          height: "60vh",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "start",
+          alignItems: "start",
+        }}
+      >
+        <Box sx={{ width: "50%" }}>
+          <Box
+            sx={{
+              display: "flex",
+              marginTop: 8,
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1, width: "60%" }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            width: "50%",
             display: "flex",
-            marginTop: 8,
             flexDirection: "column",
             alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 , width:'60%' }}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
+          <Lottie
+            animationData={anim}
+            style={{ width: "400px", height: "400px", marginTop: "60px" }}
+          />
         </Box>
       </Box>
       <Box
         sx={{
-          width: "50%",
-          height: "100vh",
           display: "flex",
           flexDirection: "column",
+
+          justifyContent: "center",
           alignItems: "center",
-          justifyContent: "space-between",
         }}
       >
-
-        <Lottie
-          animationData={anim}
-          style={{ width: "400px", height: "400px", marginTop: "60px"}}
+        <img
+          src={poste}
+          alt="logo poste"
+          style={{ width: "80px", height: "40px" }}
         />
-
-       
-      </Box>
-        </Box>
         <Typography>
-        <p className="text-center mb-1 ">
+          <p className="text-center mb-1 ">
             Copyright &copy; {new Date().getFullYear()} {""}
             All Rights Reserved. Made By <span> La Poste Tunisienne</span>
           </p>
-          <img src="" alt=""></img>
         </Typography>
-   
+      </Box>
     </Box>
   );
 }
